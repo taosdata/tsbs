1496022803a853a503e9659ad8225d10c20c8577
tangfangzhi <fztang@taosdata.com>
2022-02-21 16:42:34 +0800

    [TD-13618]<test>:install timescaleDB

---
+++ scripts/installTimescaleDB.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+# set parameters by default value
+osType=centos   # -o [centos | ubuntu]
+
+while getopts "ho:" arg
+do
+  case $arg in
+    o)
+      #echo "osType=$OPTARG"
+      osType=$(echo $OPTARG)
+      ;;
+    h)
+      echo "Usage: `basename $0` -o [centos | ubuntu]"
+      exit 0
+      ;;
+    ?) #unknow option
+      echo "unkonw argument"
+      exit 1
+      ;;
+  esac
+done
+
+function install_centos(){
+# install timescaledb in centos 
+yum install https://download.postgresql.org/pub/repos/yum/reporpms/EL-$(rpm -E %{centos})-x86_64/pgdg-redhat-repo-latest.noarch.rpm
+if [ ! -f "/etc/yum.repos.d/timescale_timescaledb.repo"  ] ;then
+  tee /etc/yum.repos.d/timescale_timescaledb.repo <<EOL
+  [timescale_timescaledb]
+  name=timescale_timescaledb
+  baseurl=https://packagecloud.io/timescale/timescaledb/el/$(rpm -E %{rhel})/\$basearch
+  repo_gpgcheck=1
+  gpgcheck=0
+  enabled=1
+  gpgkey=https://packagecloud.io/timescale/timescaledb/gpgkey
+  sslverify=1
+  sslcacert=/etc/pki/tls/certs/ca-bundle.crt
+  metadata_expire=300
+EOL
+yum update -y 
+yum remove timescaledb-2-postgresql-14 -y
+yum remove postgresql-14 -y
+yum install timescaledb-2-postgresql-14 -y
+}
+
+function install_ubuntu(){
+apt install gnupg postgresql-common apt-transport-https lsb-release wget
+/usr/share/postgresql-common/pgdg/apt.postgresql.org.sh
+curl -L https://packagecloud.io/timescale/timescaledb/gpgkey | sudo apt-key add -
+sh -c "echo 'deb https://packagecloud.io/timescale/timescaledb/ubuntu/ $(lsb_release -c -s) main' > /etc/apt/sources.list.d/timescaledb.list"
+wget --quiet -O - https://packagecloud.io/timescale/timescaledb/gpgkey | apt-key add -
+apt update
+apt install timescaledb-2-postgresql-14
+}
+
+if [ osType="centos" ];then
+  install_centos
+elif [ osType="ubuntu" ];then
+  install_ubuntu
+else
+  echo "osType can't be supported"
+fi
+
+# configure postgresql 
+sudo /usr/pgsql-14/bin/postgresql-14-setup initdb
+sudo systemctl enable postgresql-14
+sudo systemctl start postgresql-14
+
+# reset default password:password 
+su - postgres -c "psql -U postgres -c \"alter role  postgres with password 'password';\""
+echo "shared_preload_libraries = 'timescaledb'" >> /var/lib/pgsql/14/data/postgresql.conf  
+systemctl restart  postgresql-14
+PGPASSWORD=password psql -U postgres -h localhost -c "CREATE EXTENSION IF NOT EXISTS timescaledb;"
