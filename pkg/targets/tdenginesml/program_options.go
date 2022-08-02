55fe7ad87a71297bb5907a2ce62eb2b501acca8a
chenhaoran <haoran920c@163.com>
2022-08-02 17:07:10 +0800

    merge master

--- pkg/targets/tdenginesml/program_options.go
+++ pkg/targets/tdenginesml/program_options.go
@@ -1,8 +1,9 @@
 package tdenginesml
 
 type LoadingOptions struct {
-	User string
-	Pass string
-	Host string
-	Port int
+	User    string
+	Pass    string
+	Host    string
+	Port    int
+	VGroups int
 }
